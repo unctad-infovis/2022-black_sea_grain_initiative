271aaf9b37fe0fe598a18bd31696eedafe40a27d
Teemo Tebest <teemo.tebest@gmail.com>
2022-10-02 13:47:21 +0200

    Changed Datawrapper embed.

--- src/jsx/App.jsx
+++ src/jsx/App.jsx
@@ -332,7 +332,7 @@ function App() {
       { /* Table container */ }
       <div className="table_container">
         <h3>Browse the data</h3>
-        <iframe aria-label="Interactive table" frameBorder="0" height="auto" loading="lazy" scrolling="no" src="https://datawrapper.dwcdn.net/z1IW9" title="Outbound vessels" />
+        <iframe title="Vessel movements - Outbound voyages" aria-label="Table" id="datawrapper-chart-MUWoW" src="https://datawrapper.dwcdn.net/MUWoW/2/" scrolling="no" frameborder="0" style="width: 0; min-width: 100% !important; border: none;" height="931"></iframe><script type="text/javascript">!function(){"use strict";window.addEventListener("message",(function(e){if(void 0!==e.data["datawrapper-height"]){var t=document.querySelectorAll("iframe");for(var a in e.data["datawrapper-height"])for(var r=0;r<t.length;r++){if(t[r].contentWindow===e.source)t[r].style.height=e.data["datawrapper-height"][a]+"px"}}}))}();</script>
       </div>
       <noscript>Your browser does not support JavaScript!</noscript>
     </div>

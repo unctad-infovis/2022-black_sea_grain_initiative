b0fa1b287a3e744843247ca98bcdfa5246d97fa8
Teemo Tebest <teemo.tebest@gmail.com>
2023-01-19 12:40:37 +0100

    Updated data

--- src/jsx/components/LineBarChart.jsx
+++ src/jsx/components/LineBarChart.jsx
@@ -42,7 +42,7 @@ function LineBarChart({
       .attr('transform', `translate(${width}, 0)`);
   }, [margin, height]);
 
-  const xAxis = d3.axisBottom().scale(x).tickSizeOuter([0]).tickValues(['2022-08-01', '2022-08-15', '2022-09-01', '2022-09-15', '2022-10-01', '2022-10-15', '2022-11-01', '2022-11-15', '2022-12-01'])
+  const xAxis = d3.axisBottom().scale(x).tickSizeOuter([0]).tickValues(['2022-08-01', '2022-08-15', '2022-09-01', '2022-09-15', '2022-10-01', '2022-10-15', '2022-11-01', '2022-11-15', '2022-12-01', '2022-12-15', '2023-01-01', '2023-01-15'])
     .tickFormat(d => {
       const date = new Date(`${d} 12:00:00 GMT`);
       return (`${date.toLocaleString('default', { day: 'numeric' })} ${date.toLocaleString('default', { month: 'short' }).slice(0, 3)}`);

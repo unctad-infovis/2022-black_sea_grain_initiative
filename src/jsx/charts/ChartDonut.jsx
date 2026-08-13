29c8257514faf4bcfd3512c593cf7ed2f18203b9
Teemo Tebest <teemo.tebest@gmail.com>
2026-08-14 00:52:03 +0300

    Host UNCTAD logo locally instead of hardcoded unctad.org URL (blocks downloads on localhost)

--- src/jsx/charts/ChartDonut.jsx
+++ src/jsx/charts/ChartDonut.jsx
@@ -137,7 +137,7 @@ function DonutChart({
           },
           load() {
             // eslint-disable-next-line react/no-this-in-sfc
-            this.renderer.image('https://unctad.org/sites/default/files/2022-11/unctad_logo.svg', 5, 15, 80, 100).add();
+            this.renderer.image(`${window.location.href.includes('unctad.org') ? 'https://storage.unctad.org/2022-black_sea_grain_initiative/' : './'}assets/img/unctad_logo.svg`, 5, 15, 80, 100).add();
           }
         },
         height: chartHeight,
